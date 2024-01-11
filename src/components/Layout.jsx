b75b6822eaa917782193c17fe3fd8a45f686a054
alfred <lianghongbuaa@gmail.com>
2024-01-11 16:17:28 +0800

    rename

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,14 @@
 import * as React from "react"
 import { Box } from "@chakra-ui/react"
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
     <Box>
       <Header/>
       <Box as={'main'} marginTop={'80px'}>{children}</Box>
-      <footer>
-        © {new Date().getFullYear()} CopyRight
-      </footer>
+      <Footer/>
     </Box>
   )
 }
