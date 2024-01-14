600e0b2450936de6457714aecfc197666c5042c3
alfred <lianghongbuaa@gmail.com>
2024-01-14 19:45:37 +0800

    filter out pages with [wip]

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,7 +38,10 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-  const posts = result.data.allMarkdownRemark.nodes
+  let posts = result.data.allMarkdownRemark.nodes
+  posts.map(post => console.log(post.fields.slug))
+  posts = posts.filter(post => !post.fields.slug.toLowerCase().includes('[wip]'))
+
 
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
